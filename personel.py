e961e9d9543b375c9fec9bc208852977b10996b9
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-06-09 13:24:08 +0300

    def get maaş eklendi

--- personel.py
+++ personel.py
@@ -21,8 +21,13 @@ class Personel:
             return self.__soyad
         def set_soyad(self, soyad):
             self.__soyad = soyad
-            
+
         def get_departman(self):
             return self.__departman
         def set_departman(self, departman):
             self.__departman = departman
+        
+        def get_maas(self):
+            return self.__maas
+        def set_maas(self, maas):
+            self.__maas = maas
