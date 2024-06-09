e3338da1ad518e76301d822dcd1eb8eb59aab485
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-06-09 13:16:17 +0300

    class eklendi

---
+++ personel.py
@@ -0,0 +1,8 @@
+class Personel:
+    def __init__(self, personel_no, ad, soyad, departman, maas):
+        self.__personel_no = personel_no
+        self.__ad = ad
+        self.__soyad = soyad
+        self.__departman = departman
+        self.__maas = maas
+#class personel oluşturuldu
