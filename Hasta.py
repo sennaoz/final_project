57860cbf7e62ca612d7494069fbe4517e069efca
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-06-09 16:34:54 +0300

    class oluşturuldu

---
+++ Hasta.py
@@ -0,0 +1,10 @@
+class Hasta:
+    def __init__(self, hasta_no, ad, soyad, dogum_tarihi, hastalik, tedavi):
+        self.__hasta_no = hasta_no
+        self.__ad = ad
+        self.__soyad = soyad
+        self.__dogum_tarihi = dogum_tarihi
+        self.__hastalik = hastalik
+        self.__tedavi = tedavi
+        
+    
