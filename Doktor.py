82c5e6c919d579a73d9c655e1e8a828ff048a804
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-06-09 13:35:08 +0300

    class doktor oluşturuldu personel miras alındı

--- Doktor.py
+++ Doktor.py
@@ -1,1 +1,7 @@
 import Personel
+class Doktor(Personel):
+    def __init__(self, personel_no, ad, soyad, departman, maas, uzmanlik, deneyim_yili, hastane):
+        super().__init__(personel_no, ad, soyad, departman, maas)
+        self.__uzmanlik = uzmanlik
+        self.__deneyim_yili = deneyim_yili
+        self.__hastane = hastane
