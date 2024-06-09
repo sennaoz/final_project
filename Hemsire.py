d88a77bea2add242429edf3c8c29f91ac80c1980
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-06-09 14:48:55 +0300

    class hemsire eklendi miras personel eklendi

--- Hemsire.py
+++ Hemsire.py
@@ -1,1 +1,7 @@
 import Personel
+class Hemsire(Personel):
+    def __init__(self, personel_no, ad, soyad, departman, maas, calisma_saati, sertifika, hastane):
+        super().__init__(personel_no, ad, soyad, departman, maas)
+        self.__calisma_saati = calisma_saati
+        self.__sertifika = sertifika
+        self.__hastane = hastane
