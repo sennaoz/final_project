d874ed64aa222ab92e79f14af654dae888b8a3d4
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-06-11 23:23:02 +0300

    personel_bilgileri_iste eklendi

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from Personel import Personel
 from Doktor import Doktor
 from Hemsire import Hemsire
 from Hasta import Hasta
+
+def personel_bilgileri_iste():
+    personel_no = input("No: ")
+    ad = input("Ad: ")
+    soyad = input("Soyad: ")
+    departman = input("Departman: ")
+    maas = float(input("Maaş: "))
+    return personel_no, ad, soyad, departman, maas
